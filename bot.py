1564fe65a0748ac469d2378da1444877ee2003ca
grito666 <valeriy.petrov2018@gmail.com>
2025-06-06 12:20:01 +0400

    bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ TOKEN = os.getenv("TELEGRAM_TOKEN")
 async def start(update, context):
     await update.message.reply_text("Привет! Я работаю!")
 
-if name == "__main__":
+if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     print("Бот запущен...")
